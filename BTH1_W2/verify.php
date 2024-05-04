ab4c5ba48d9c573988f5cde448ff7bfc48df3eb5
Nkhanh218 <exodiakhanh@gmail.com>
2024-05-04 18:06:27 +0700

    Test

--- BTH1_W2/verify.php
+++ BTH1_W2/verify.php
@@ -3,9 +3,9 @@ session_start();
 
 // Database connection
 $db_host = 'localhost';
-$db_user = 'your_username';
-$db_pass = 'your_password';
-$db_name = 'your_database';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'users';
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (!$conn) {
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Username and password are correct, start a new session
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
-        header("Location: welcome.php");
+        header("Location: home.php");
         exit;
     } else {
         // Incorrect username or password
