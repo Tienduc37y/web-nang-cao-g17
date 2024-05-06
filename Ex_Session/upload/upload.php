2b75753f5720a0c832682028c4d627c69528ec43
Lại Tiến Đức <94907773+Tienduc37y@users.noreply.github.com>
2024-05-06 15:24:59 +0000

    giới hạn dung lượng upload file

--- Ex_Session/upload/upload.php
+++ Ex_Session/upload/upload.php
@@ -8,14 +8,24 @@ if(isset($_POST["submit"])) {
         $sizes = $files['size'];
         $numitems = count($names);
         $numfiles = 0;
+
         for ($i = 0; $i < $numitems; $i++) {
             if ($errors[$i] == 0) {
-                $numfiles++;
-                $uploadDate = filemtime($tmp_names[$i]);
-                echo "<b>File $numfiles:</b><br>";
-                echo "Ngày upload: " . date("Y-m-d H:i:s", $uploadDate) . "<br>";
-                echo "Tên: $names[$i] <br>";
-                echo "Kích Thước: $sizes[$i] bytes<br><hr>";
+                if($sizes[$i] < 2097152){
+                        $numfiles++;
+                        $uploadDate = filemtime($tmp_names[$i]);
+                        echo "<b>File $numfiles:</b><br>";
+                        echo "Ngày upload: " . date("Y-m-d H:i:s", $uploadDate) . "<br>";
+                        echo "Tên: $names[$i] <br>";
+                        echo "Kích Thước: $sizes[$i] bytes<br><hr>";
+                        echo '<form method="post">';
+                        echo '<input type="hidden" name="delete_index" value="' . $i . '">';
+                        echo '<input type="submit" name="delete_file" value="Xóa">';
+                        echo '</form>';
+                    }
+                }
+                else {
+                    echo "<h1>Vượt quá dung lượng giới hạn</h1>";
             }
         }
         echo "Số file upload : " .$numfiles . "<br>";
